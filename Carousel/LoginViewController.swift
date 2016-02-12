fde9b04807a312765dc57c63390d64cd5fed6644
Jenn Leung <jleung@jleung.local>
2016-02-11 23:38:41 -0800

    added field outlets

--- Carousel/LoginViewController.swift
+++ Carousel/LoginViewController.swift
@@ -13,6 +13,14 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
     var buttonInitialY: CGFloat!
     var buttonOffset: CGFloat!
     @IBOutlet weak var buttonParentView: UIView!
+    @IBOutlet weak var fieldParentView: UIView!
+    @IBOutlet weak var loginNavBar: UIImageView!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var signinIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +32,29 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
         buttonInitialY = buttonParentView.frame.origin.y
         buttonOffset = -120
         
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Set initial transform values 20% of original size
+        let transform = CGAffineTransformMakeScale(0.2, 0.2)
+        // Apply the transform properties of the views
+        loginNavBar.transform = transform
+        fieldParentView.transform = transform
+        // Set the alpha properties of the views to transparent
+        loginNavBar.alpha = 0
+        fieldParentView.alpha = 0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        //Animate the code within over 0.3 seconds...
+        UIView.animateWithDuration(0.3) { () -> Void in
+            // Return the views transform properties to their default states.
+            self.fieldParentView.transform = CGAffineTransformIdentity
+            self.loginNavBar.transform = CGAffineTransformIdentity
+            // Set the alpha properties of the views to fully opaque
+            self.fieldParentView.alpha = 1
+            self.loginNavBar.alpha = 1
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +77,9 @@ class LoginViewController: UIViewController, UIScrollViewDelegate {
         scrollViewDidScroll(scrollView)
     }
     
+    @IBAction func didPressSignin(sender: AnyObject) {
+        
+    }
     func scrollViewDidScroll(scrollView: UIScrollView) {
         print("scroll")
         // If the scrollView has been scrolled down by 50px or more...
