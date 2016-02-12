74b72f40cc10c6c12714946ccfdda16b75a3e21c
Jenn Leung <jleung@jleung.local>
2016-02-12 01:51:34 -0800

    finished tutorial screens

--- Carousel/TutorialViewController.swift
+++ Carousel/TutorialViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class TutorialViewController: UIViewController {
+class TutorialViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var buttonParentView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: 1280, height: 568)
+        scrollView.delegate = self
+        buttonParentView.alpha = 0
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // Get the current page based on scroll event
+        let page : Int = Int(round(scrollView.contentOffset.x / 320))
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
+        
+        if page == 3 {
+            pageControl.hidden = true
+            UIView.animateWithDuration(0.3) { () -> Void in
+                self.buttonParentView.alpha = 1
+                self.pageControl.hidden = true
+            }
+        }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
