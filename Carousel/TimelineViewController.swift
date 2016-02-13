4189a269b2326211947d230776764b652be4c9c3
Jenn Leung <jleung@jleung.local>
2016-02-12 20:08:02 -0800

    finished requirements as well as a couple optionals

---
+++ Carousel/TimelineViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimelineViewController.swift
+//  Carousel
+//
+//  Created by Jenn Leung on 2/12/16.
+//  Copyright © 2016 Jenn Leung. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var conversationButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1568)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
